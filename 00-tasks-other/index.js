bfd28c5f84e1c5ea5a7e7ec5e7b8d3d8b4569315
Leva <parimalleva@gmail.com>
2022-08-23 15:46:22 +0300

    add tasks 00-tasks in index.js

--- 00-tasks-other/index.js
+++ 00-tasks-other/index.js
@@ -1,28 +1,103 @@
-// Создать функцию которая принимает массив футболистов
+// === Part I ===
+
+// 1. Дан случайный массив имен. Выяснить есть ли в этом
+// массиве «Семён» с помощью метода .some().
+
+// const arrayName = [
+//   "Олег",
+//   "Игорь",
+//   "Семен",
+//   "Дмитрий",
+//   "Антон",
+//   "Семен",
+//   "Андрей",
+// ];
+
+// let checkName = arrayName.some((element) => element === "Семен");
+// console.log(checkName);
+
+// 2. Дан массив случайных чисел. С помощью метода
+// .reduce() просуммировать только четные числа из этого
+// массива.
+
+// const arrNumber = [2, 1, 3, 111, 2, 2, 42, 123];
+// let sum = arrNumber.reduce((acc, item) => (!(item % 2) ? acc + item : acc), 0);
+// console.log(sum);
+
+// 3. Пользователь вводит 10 случайных значений. Каждое
+// значение необходимо записать в массив. С помощью
+// метода .every() проверить были ли все введенные
+// пользователем данные – числами.
+
+// let arrNumber = new Array(10);
+// for (let i = 0; i < arrNumber.length; i++) {
+//   arrNumber[i] = prompt("Pleace enter number");
+// }
+// console.log(arrNumber);
+
+// const result = arrNumber.every((item) => !isNaN(item));
+// console.log(result);
+
+// 4. Создать объект check с двумя методами:
+// .checkNameLength()
+// .checkIsAdult()
+// создать два объекта person1 и person2 с полями name и age (задать самостоятельно)
+// вызвать методы из объекта check и передать в них в качестве контекста
+// person1 или person2.
+// checkNameLength() должен возвращать true если поле name у объекта больше 4
+// checkIsAdult() должен возвращать true если поле объекта age больше или равно 18
+
+// const check = {
+//     checkNameLength() {
+//         return this.name.length > 4;
+//     },
+//     checkIsAdult() {
+//         return this.age >= 18;
+//     }
+// };
+
+// const person1 = {
+//     name: 'Oleg',
+//     age: 32,
+// };
+
+// const person2 = {
+//     name: 'Vasenlisa',
+//     age: 16,
+// };
+
+// console.log(check.checkNameLength.call(person1));
+// console.log(check.checkNameLength.call(person2));
+// console.log(check.checkIsAdult.call(person1));
+// console.log(check.checkIsAdult.call(person2));
+
+// 5. Создать объект person с полем name и методом eat.
+// Создать массив строк food в котором перечисляется любая еда (не менее 4 наименований)
+// Метод eat должен принимать строку food с названием еды и возвращать строку вида
+// person.name + " eats " + food
+// Используя setTimeout или setInterval запустите метод eat объекта person раз в секунду,
+// передавая при этом каждый раз следующее название еды из массива.
+
+const person = {
+    name: 'Lena',
+    eat(food) {
+        return `${person.name} eats ${food}`;
+    },
+}
+
+let food = ['sushi', 'salad', 'vodka', 'strawberry'];
+let count = 0;
+let id = setInterval(() => {
+    console.log(person.eat(food[++count]));
+}, 1000);
+
+setTimeout(() => { clearInterval(id) }, 4000);
+
+// === Part II ===
+// 1. Создать функцию которая принимает массив футболистов
 // Функция должна "пробегать" по массиву и выяснять какие футболисты играют в одинаковых командах.
 // функция должна возвращать двумерный массив футболистов сформированный по принципу: 1 массив - 1 команда
 // пример возвращаемого массива:
-// const players = [
-//     { name: "Ronaldo", team: "Juventus" },
-//     { name: "Messi", team: "Barcelona" },
-//     { name: "Pedro", team: "Chelsea" },
-//     { name: "Rico", team: "PSG" },
-//     { name: "Suarez", team: "Barcelona" },
-//     { name: "Buffon", team: "Juventus" },
-//     { name: "Zuma", team: "Chelsea" },
-//     { name: "Gonsalo", team: "Juventus" },
-// ];
-
-// function playersSameTeam(players) {
-//     const arr = [];
-
-//     for(players[key] in players[key]);
-
-//     return arr;
-// }
-
-// console.log(playersSameTeam(players));
-
 // [
 // 	[
 // 		{
@@ -66,6 +141,42 @@
 // 		}
 // 	]
 // ]
+
+// const players = [
+//     { name: "Ronaldo", team: "Juventus" },
+//     { name: "Messi", team: "Barcelona" },
+//     { name: "Pedro", team: "Chelsea" },
+//     { name: "Rico", team: "PSG" },
+//     { name: "Suarez", team: "Barcelona" },
+//     { name: "Buffon", team: "Juventus" },
+//     { name: "Zuma", team: "Chelsea" },
+//     { name: "Gonsalo", team: "Juventus" },
+// ];
+
+// function sortPlayers(players) {
+
+//     const sortedPlayers = [];
+//     const sortedByTeamName = players.sort(
+//         (a, b) => a.team > b.team ? 1 : (b.team > a.team) ? -1 : 0
+//     );
+
+//     sortedPlayers.push(
+//         sortedByTeamName.reduce((team, player) => {
+//             if (team[0]?.team === player.team || team.length === 0) {
+//                 team.push(player);
+//                 return team;
+//             } else {
+//                 sortedPlayers.push(team)
+//                 return [player]
+//             }
+//         }, [])
+//     );
+
+//     return sortedPlayers;
+// }
+
+// console.log(sortPlayers(players));
+
 
 // // 2.
 // const car = {
@@ -156,57 +267,6 @@
 // 2.3 найти хотя бы одно авто мощность двигателя которого больше 500
 // 2.4 найти самое дешевое авто по наименьшей цене
 
-// 2. Дан случайный массив имен. Выяснить есть ли в этом
-// массиве «Семён» с помощью метода .some().
 
-// const arrayName = [
-//   "Олег",
-//   "Игорь",
-//   "Семен",
-//   "Дмитрий",
-//   "Антон",
-//   "Семен",
-//   "Андрей",
-// ];
 
-// let checkName = arrayName.some((element) =>
-//   element === "Семен" ? true : false
-// );
-// console.log(checkName);
 
-// 3. Дан массив случайных чисел. С помощью метода
-// .reduce() просуммировать только четные числа из этого
-// массива.
-
-const arrNumber = [2, 1, 3, 111, 2, 2, 42, 123];
-let sum = arrNumber.reduce((acc, item) => (acc += item % 2 ? 0 : item), 0);
-console.log(sum);
-
-// 4. Пользователь вводит 10 случайных значений. Каждое
-// значение необходимо записать в массив. С помощью
-// метода .every() проверить были ли все введенные
-// пользователем данные – числами.
-
-// let arrNumber = new Array(10);
-// for (let i = 0; i < arrNumber.length; i++) {
-//   arrNumber[i] = prompt("Pleace enter number");
-// }
-// console.log(arrNumber);
-
-// const result = arrNumber.every((item) => !isNaN(item));
-// console.log(result);
-
-// 1. Создать объект check с двумя методами:
-// .checkNameLength()
-// .checkIsAdult()
-// создать два объекта person1 и person2 с полями name и age (задать самостоятельно)
-// вызвать методы из объекта check и передать в них в качестве контекста
-// person1 или person2.
-// checkNameLength() должен возвращать true если поле name у объекта больше 4
-// checkIsAdult() должен возвращать true если поле объекта age больше или равно 18
-
-// 2. Создать объект person с полем name и методом eat.
-// Создать массив строк food в котором перечисляется любая еда (не менее 4 наименований)
-// Метод eat должен принимать строку food с названием еды и возвращать строку вида
-// person.name + " eats " + food
-// Используя setTimeout или setInterval запустите метод eat объекта person раз в секунду, передавая при этом каждый раз следующее название еды из массива.
