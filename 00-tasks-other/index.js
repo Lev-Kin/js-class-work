3fac1e9905d3d3fa9c660e6bb323ee89414b16a6
Leva <parimalleva@gmail.com>
2022-08-25 15:39:01 +0300

    fix tasks 2.4

--- 00-tasks-other/index.js
+++ 00-tasks-other/index.js
@@ -235,7 +235,7 @@ const findLowerPriceCar = arrCars.
     find(({ specs: { price: { low } } }) =>
         low === arrCars.
             reduce((acc, { specs: { price: { low } } }) =>
-                acc < low ? acc : low));
+                (acc < low ? acc : low), low));
 
 console.log(findLowerPriceCar);
 
