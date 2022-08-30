0c690af4216fe9eedf42058d432b6e7df174a1c2
Web <web@it.step>
2022-08-30 21:17:37 +0300

    add tasks callback-closures

--- 08-callbacks-closures/tasks.js
+++ 08-callbacks-closures/tasks.js
@@ -226,16 +226,16 @@
 //   };
 // }
 
-function add(a, b) {
-  return a + b;
-}
+// function add(a, b) {
+//   return a + b;
+// }
 
-function addWithLongging(a, b) {
-  console.log(a, b);
-  return add(a, b);
-}
+// function addWithLongging(a, b) {
+//   console.log(a, b);
+//   return add(a, b);
+// }
 
-console.log(addWithLongging(2, 3))
+// console.log(addWithLongging(2, 3))
 
 // const wrapWithLogger = (add) => {
 //     return (a,b) => {
@@ -244,32 +244,129 @@ console.log(addWithLongging(2, 3))
 //     };
 // };
 
+// const wrapWithLogger = (add) => {
+//     return (...args) => {
+//         console.log(...args);
+//         return add(...args);
+//     };
+// };
 
-const wrapWithLogger = (add) => {
-    return (...args) => {
-        console.log(...args);
-        return add(...args);
-    };
-};
+// function sub (a, b) {
+//     return a - b;
+// }
 
-function sub (a, b) {
-    return a - b;
+// function replaceLast (arr, str, length) {
+//     arr.pop()
+//     arr.push(str);
+//     arr.length = length;
+//     return arr;
+// }
+
+// const addLogger = wrapWithLogger(add);
+// const subLogger = wrapWithLogger(sub);
+// const replaceLastLogger = wrapWithLogger(replaceLast)
+
+// //console.log(addLogger(2,3));
+// console.log(subLogger(2, 3));
+// console.log(replaceLastLogger(['a', 'b', 'c'], 'd', 20));
+
+// ===========================================================
+// 1. Создать функцию «переключатель» toggleMe(),
+// которая при вызове чередует возвращаемые булевы
+// значения (true, false).
+
+// function toggleMe() {
+//   let a = true;
+//   return function foo() {
+//     return a = !a;
+//   };
+// }
+
+// let toggler = toggleMe();
+
+// console.log(toggler());
+// console.log(toggler());
+// console.log(toggler());
+// console.log(toggler());
+// console.log(toggler());
+// console.log(toggler());
+
+// 2. Создать функцию funcCounter(), которая способна
+// считать количество своих вызовов. Если передать в эту
+// функцию число меньше 10, то функция считает
+// количество вызовов от этого числа до бесконечности,
+// если передать число больше 10, то функция считает
+// количество от этого числа до 0
+
+function funcCounter(num) {
+  let count = num;
+  return function () {
+    if (num > 10) {
+      return count++;
+    } else {
+      return count--;
+    }
+  };
 }
 
-function replaceLast (arr, str, length) {
-    arr.pop()
-    arr.push(str);
-    arr.length = length;
-    return arr;
-}
+let c = funcCounter(11);
 
-const addLogger = wrapWithLogger(add);
-const subLogger = wrapWithLogger(sub);
-const replaceLastLogger = wrapWithLogger(replaceLast)
+// while(c() > 10){
+//   console.log(c());
+// }
 
-//console.log(addLogger(2,3));
-console.log(subLogger(2, 3));
-console.log(replaceLastLogger(['a', 'b', 'c'], 'd', 20));
+console.log(c());
+console.log(c());
+console.log(c());
+console.log(c());
 
 
+// console.log(funcCounter(9));
+// 3. Создать функцию classNameEditor(), которая
+// принимает строку с названием класса и добавляет
+// его в класс, если вторым параметром было
+// передано true, если вторым параметром было
+// передано false, то функция пытается удалить из
+// класса переданное имя. Если удаление не было
+// совершено, то просто вернуть строку с названием
+// класса. Функция должна возвращать текущее
+// название класса.
 
+// classNameEditor('active', true) // добавляем класс ====>>>>> 'active'
+// classNameEditor('test', true) // =====>>>>> 'active test'
+// classNameEditor('btn', true) // =====>>>>> 'active test btn'
+// classNameEditor('test', false) // =====>>>>> 'active btn'
+// classNameEditor('blabla', false) // =====>>>>> 'active btn'
+
+// .split()
+// .join()
+
+// 4. Создать функцию, которая принимает массив и callback. Функция
+// должна каждый элемент массива обрабатывать callback’ом.
+
+// arr.map(() => {})
+// map(arr, () => {})
+
+// 5. Создать функцию sumTo, которая принимает два числа и callback.
+// Задача функции складывать 2 числа, а задача callback’a заставлять
+// функцию sumTo возвращать результат в виде числа, строки или
+// логического типа данных.
+
+// function toString (val) { return val + ''; }
+
+// sumTo(2,3, someCallback)
+
+// 6. Создать функцию checkLogin, которая принимает 4 параметра. 1
+// параметр - это логин, введенный пользователем, 2 –
+// правильный логин, 3 – callback, который вызовется если логин
+// верный, 4 – callback, который вызовется если логин ложный.
+
+// 7. Создать объект
+// kurochka = {
+// 	name: 'Ryaba',
+// 	isAlive: true,
+// 	isZazharena: false
+// }
+
+// измените объект так, чтобы после изменения поля isZazharena = true, поле isAlive автоматически стало false,
+// поле isAlive должно быть доступно только для чтения. После изменения isZazharena - это поле должно стать доступно только для чтения
