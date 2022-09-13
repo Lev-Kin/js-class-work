23832c93e86a469091c912dc68e54e8e9a505ff7
Leva <parimalleva@gmail.com>
2022-09-13 12:27:07 +0300

    add theory 09-prototype

--- 09-prototype/tasks.js
+++ 09-prototype/tasks.js
@@ -105,4 +105,148 @@
 // Остальные методы получения ключей/значений работают только с собственными свойствами объекта.
 
 //======= 1.2 F.prototype =======
+// let animal = {
+//     eats: true
+// };
+
+// function Rabbit(name) {
+//     this.name = name;
+// }
+
+// Rabbit.prototype = animal;
+
+// let rabbit = new Rabbit("White Rabbit"); //  rabbit.__proto__ == animal
+
+// alert(rabbit.eats); // true
+
+//=== F.prototype по умолчанию, свойство constructor
+// function Rabbit() { }
+// // по умолчанию:
+// // Rabbit.prototype = { constructor: Rabbit }
+
+// let rabbit = new Rabbit(); // наследует от {constructor: Rabbit}
+
+// alert(rabbit.constructor == Rabbit); // true (свойство получено из прототипа)
+
+//=== Итого
+// Свойство F.prototype (не путать с [[Prototype]]) устанавливает[[Prototype]] для новых объектов при вызове new F().
+// Значение F.prototype должно быть либо объектом, либо null. Другие значения не будут работать.
+// Свойство "prototype" является особым, только когда оно назначено функции-конструктору, которая вызывается оператором new.
+
+//======= 1.3 Встроенные прототипы =======
+
+// let obj = {};
+
+// alert(obj.__proto__ === Object.prototype); // true
+// // obj.toString === obj.__proto__.toString === Object.prototype.toStringlet obj = {};
+
+// alert(obj.__proto__ === Object.prototype); // true
+// // obj.toString === obj.__proto__.toString === Object.prototype.toString
+
+// alert(Object.prototype.__proto__); // null
+
+// //=== Другие встроенные прототипы
+// let arr = [1, 2, 3];
+
+// // наследует ли от Array.prototype?
+// alert(arr.__proto__ === Array.prototype); // true
+
+// // затем наследует ли от Object.prototype?
+// alert(arr.__proto__.__proto__ === Object.prototype); // true
+
+// // и null на вершине иерархии
+// alert(arr.__proto__.__proto__.__proto__); // null
+
+// // === Изменение встроенных прототипов
+// if (!String.prototype.repeat) { // Если такого метода нет
+//     // добавляем его в прототип
+
+//     String.prototype.repeat = function (n) {
+//         // повторить строку n раз
+
+//         // на самом деле код должен быть немного более сложным
+//         // (полный алгоритм можно найти в спецификации)
+//         // но даже неполный полифил зачастую достаточно хорош для использования
+//         return new Array(n + 1).join(this);
+//     };
+// }
+
+// alert("La".repeat(3)); // LaLaLa
+
+// //=== Заимствование у прототипов
+// let obj = {
+//     0: "Hello",
+//     1: "world!",
+//     length: 2,
+// };
+
+// obj.join = Array.prototype.join;
+
+// alert(obj.join(',')); // Hello,world!
+
+//=== Итого
+// - Все встроенные объекты следуют одному шаблону:
+//   - Методы хранятся в прототипах (Array.prototype, Object.prototype, Date.prototype и т.д.).
+//   - Сами объекты хранят только данные (элементы массивов, свойства объектов, даты).
+// - Примитивы также хранят свои методы в прототипах объектов-обёрток:
+// Number.prototype, String.prototype, Boolean.prototype. Только у значений undefined и null нет объектов-обёрток.
+// - Встроенные прототипы могут быть изменены или дополнены новыми методами.
+// Но не рекомендуется менять их. Единственная допустимая причина – это добавление нового метода из стандарта,
+// который ещё не поддерживается движком JavaScript.
+// Function.prototype.defer = function (ms) {
+//     setTimeout(this, ms);
+// };
+
+// function f() {
+//     alert("Hello!");
+// }
+
+// f.defer(1000); // выведет "Hello!" через 1 секунду
+
+//======= 1.4 Методы прототипов, объекты без свойства __proto__ =======
+// let animal = {
+//     eats: true
+// };
+
+// // создаём новый объект с прототипом animal
+// let rabbit = Object.create(animal);
+
+// alert(rabbit.eats); // true
+
+// alert(Object.getPrototypeOf(rabbit) === animal); // получаем прототип объекта rabbit
+
+// Object.setPrototypeOf(rabbit, {}); // заменяем прототип объекта rabbit на {}
+
+// let animal = {
+//     eats: true
+// };
+
+// let rabbit = Object.create(animal, {
+//     jumps: {
+//         value: true
+//     }
+// });
+
+// alert(rabbit.jumps); // true
+
+// Object.create(proto, [descriptors]) – создаёт пустой объект со свойством [[Prototype]], 
+// указанным как proto, и необязательными дескрипторами свойств descriptors.
+// Object.getPrototypeOf(obj) – возвращает свойство [[Prototype]] объекта obj.
+// Object.setPrototypeOf(obj, proto) – устанавливает свойство [[Prototype]] объекта obj как proto.
+
+// let chineseDictionary = Object.create(null);
+// chineseDictionary.hello = "你好";
+// chineseDictionary.bye = "再见";
+
+// alert(Object.keys(chineseDictionary)); // hello,bye
+
+// let clone = Object.create(Object.getPrototypeOf(obj), Object.getOwnPropertyDescriptors(obj));
+
+// Ещё методы:
+// Object.keys(obj) / Object.values(obj) / Object.entries(obj)
+// – возвращают массив всех перечисляемых собственных строковых ключей/значений/пар ключ-значение.
+// Object.getOwnPropertySymbols(obj) – возвращает массив всех собственных символьных ключей.
+// Object.getOwnPropertyNames(obj) – возвращает массив всех собственных строковых ключей.
+// Reflect.ownKeys(obj) – возвращает массив всех собственных ключей.
+// obj.hasOwnProperty(key): возвращает true, если у obj есть собственное (не унаследованное) свойство с именем key.
 
